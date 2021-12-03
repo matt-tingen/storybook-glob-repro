90bde193bbf06d2725eacdb22e909674985c13d8
Matt Tingen <matt.tingen@gmail.com>
2021-12-03 17:55:03 -0500

    avoid resolve

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,16 +1,9 @@
-const path = require('path');
-
-const rootDir = path.resolve(__dirname, '../../..');
-const storiesGlob = path.join(
-  rootDir,
-  'packages/foo-bar/{src,stories}/**/*.stories.{js,jsx,ts,tsx}', // No stories found
-  // 'packages/foo-bar/@(src|stories)/**/*.stories.@(js|jsx|ts|tsx)', // No stories found
-  // 'packages/*/{src,stories}/**/*.stories.{js,jsx,ts,tsx}', // Okay
-  // 'packages/foo-bar/src/**/*.stories.{js,jsx,ts,tsx}', // Okay
-);
+const storiesGlob =
+  '../../../packages/foo-bar/{src,stories}/**/*.stories.{js,jsx,ts,tsx}'; // No stories found
+// '../../../packages/*/{src,stories}/**/*.stories.{js,jsx,ts,tsx}'; // Okay
+// '../../../packages/foo-bar/src/**/*.stories.{js,jsx,ts,tsx}'; // Okay
 
 console.log({
-  rootDir,
   storiesGlob,
 });
 
