8824f404864fe95f374123ae2d1c111456ba17d8
Matt Tingen <matt.tingen@gmail.com>
2021-12-03 17:46:29 -0500

    repro

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,5 +1,21 @@
+const path = require('path');
+
+const rootDir = path.resolve(__dirname, '../../..');
+const storiesGlob = path.join(
+  rootDir,
+  'packages/foo-bar/{src,stories}/**/*.stories.{js,jsx,ts,tsx}', // No stories found
+  // 'packages/foo-bar/@(src|stories)/**/*.stories.@(js|jsx|ts|tsx)', // No stories found
+  // 'packages/*/{src,stories}/**/*.stories.{js,jsx,ts,tsx}', // Okay
+  // 'packages/foo-bar/src/**/*.stories.{js,jsx,ts,tsx}', // Okay
+);
+
+console.log({
+  rootDir,
+  storiesGlob,
+});
+
 module.exports = {
-  stories: ['../../foo-bar/src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [storiesGlob],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/react',
 };
